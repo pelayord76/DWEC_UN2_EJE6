cfd02ec47add4f067a1867a1dba4eb96f9dfc3d6
pelayord76 <pelayord76@educastur.es>
2023-10-06 13:02:12 +0200

    solucion

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
-var paises = ["España", "Portugal", "Alemania"];
+run();
 
-console.log(mostrarElementos(paises));
-
-function mostrarElementos(array) {
-    let resultado = "";
-    for (var indice of array) {
-        resultado += indice + "\n";
+function mostrarArray(paises){
+    for (pais of paises)
+        console.log(pais+"\n");
     }
-    return resultado;
-}
 
-function mostrarElementosInvertidos(array){
+    function mostrarArrayInverso(paises){
+        paises.reverse().forEach(function(valor){
+                                     console.log(valor+"\n")})
+    }
+
+    function mostrarArrayOrdenado(paises){
+        console.log(paises.sort().join("\n"));
+    }
     
-}
+    function anadirPaisAlPrincipio(pais, paises){
+        paises.unshift(pais);
+        console.log(paises.toString());
+    }
+
+    function anadirPaisAlFinal(pais, paises){
+    paises.push(pais);
+    console.log(paises.toString());
+    }
+    
+    function borrarAlComienzo(paises){
+        console.log(paises.shift());
+    }
+    
+    function borrarAlFinal(paises){
+        console.log(paises.pop());
+    }
+    
+    function run(){
+        var misPaises=["Japón", "Italia", "Australia", "Sudáfrica", "India", "Argentina"]
+        console.log("**********   mostrarArray   ***********");
+        mostrarArray(misPaises);
+        console.log("**********   mostrarInverso   ***********");
+        mostrarInverso(misPaises);
+        console.log("**********   mostrarOrdenado   ***********");
+        mostrarOrdenado(misPaises);
+        console.log("**********   anadirPaisPrincipio   ***********");
+        anadirPaisPrincipio("Canadá", misPaises);
+        console.log("**********   anadirPaisFinal   ***********");
+        anadirPaisFinal("Grecia", misPaises);
+        console.log("**********   borrarComienzo   ***********");
+        borrarComienzo(misPaises);
+        console.log("**********   borrarFinal   ***********");
+        borrarFinal(misPaises);
+        }
